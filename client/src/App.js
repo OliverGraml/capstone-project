55a3287989dd06f62da4427e1b750f29098a7604
Oliver Graml <o.graml@gmx.de>
2021-06-04 14:43:36 +0200

    implemented the basic-server connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,15 @@
+import {useEffect, useState} from 'react';
+
 function App() {
-  return <div className="App"></div>;
+  const [serverMessage, setServerMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:4000/')
+      .then((res) => res.json())
+      .then((res) => setServerMessage(res));
+  });
+
+  return <div className="App">{serverMessage}</div>;
 }
 
 export default App;
